670dc24f7fdcaa33376c5b48f58c0261bec2abff
James A Cone <jacone626@gmail.com>
2023-03-23 16:03:27 -0400

    added license badges

---
+++ README_sample.md
@@ -0,0 +1,41 @@
+# my project
+![License: Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+## Description
+  
+this is a good project
+  
+## Table of Contents 
+  
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [How to Contribute](#how-to-contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+  
+## Installation
+  
+ddd
+  
+## Usage
+  
+dddd
+  
+## License
+  
+Apache License 2.0
+  
+## How to Contribute
+  
+dd
+  
+## Tests
+  
+g
+  
+## Questions
+
+Github profile: github.com/jacone626
+
+For additiional questions, please contact me at jacone97@aol.com.
--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const questions = function() {
         type: 'list',
         message: 'Please choose your license for this project.',
         name: "license",
-        choices: ["Apache", "lit", "x"],
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal' ,'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
       },
       {
         type: 'input',
@@ -39,7 +39,7 @@ const questions = function() {
       },
       {
         type: 'input',
-        message: 'Please provides tests for your project and examples of how to run them.',
+        message: 'Please provide tests for your project and examples of how to run them.',
         name: "test",
       },
       {
